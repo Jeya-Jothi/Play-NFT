d541c84d40ba8f99b5b98bd249466fd2f78fcbe5
Jeya-Jothi <joe@zyppyit.com>
2026-02-13 12:57:06 +0530

    Added moving hand with responsive

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -3,6 +3,7 @@ import BrandMarquee from "../Components/HomePage/BrandMarquee.jsx";
 import Popular from "../Components/HomePage/Popular.jsx";
 import TopSeller from "../Components/HomePage/TopSellers.jsx";
 import Explore from "../Components/HomePage/Explore.jsx";
+import HandAnimate from "../Components/HandAnimate.jsx";
 
 export default function HomePage() {
   return (
@@ -13,6 +14,7 @@ export default function HomePage() {
         <Popular />
         <TopSeller />
         <Explore />
+        <HandAnimate />
       </section>
     </>
   );
